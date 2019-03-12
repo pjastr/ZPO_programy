3c1555b300d7005c20a770f4dab378a3b470c87e
Piotrek <24464854+pjastr@users.noreply.github.com>
2019-03-12 20:58:34 +0100

    adapter

---
+++ AdapterWzorzecProjektowy/AdapterWzorzecProjektowy/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdapterWzorzecProjektowy
+{
+    // kod pochodzi ze strony http://www.altcontroldelete.pl/artykuly/wzorzec-adapter-przykladowa-implementacja-w-c/
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SomeStrangeClass obj = new SomeStrangeClass();
+            IListener listener = new SomeStrangeClassAdapter(obj);
+            listener.Notify("Testowa wiadomość!!!");
+            Console.ReadKey();
+        }
+    }
+
+
+    public class SomeStrangeClass
+    {
+        public void DoSomeStrangeThing(string info)
+        {
+            Console.WriteLine(info);
+        }
+    }
+
+    public interface IListener
+    {
+        void Notify(string msg);
+    }
+
+    public class SomeStrangeClassAdapter : IListener
+    {
+        private SomeStrangeClass adaptee = null;
+
+        public SomeStrangeClassAdapter(SomeStrangeClass adaptee)
+        {
+            this.adaptee = adaptee;
+        }
+
+        public void Notify(string msg)
+        {
+            // Optionally - do something else
+            this.adaptee.DoSomeStrangeThing(msg);
+        }
+    }
+}
