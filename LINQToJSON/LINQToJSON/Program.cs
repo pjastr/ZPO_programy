99942075e77de17249bbb4cbeda31a800544191b
pjastr <24464854+pjastr@users.noreply.github.com>
2019-04-02 22:36:29 +0200

    fix

---
+++ LINQToJSON/LINQToJSON/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json.Linq;
+
+namespace LINQToJSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JObject o = JObject.Parse(@"{
+                'CPU': 'Intel',
+                'Drives': [
+                    'DVD read/writer',
+                    '500 gigabyte hard drive'
+                ]
+            }");
+
+            string cpu = (string)o["CPU"];
+            // Intel
+
+            string firstDrive = (string)o["Drives"][0];
+            // DVD read/writer
+
+            IList<string> allDrives = o["Drives"].Select(t => (string)t).ToList();
+            // DVD read/writer
+            // 500 gigabyte hard drive
+        }
+    }
+}
