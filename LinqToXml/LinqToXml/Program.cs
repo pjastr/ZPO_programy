70aa2bcda21a0a9ff4aa4bfcdd1ad42f3b476b29
pjastr <24464854+pjastr@users.noreply.github.com>
2019-04-02 21:48:18 +0200

    linqtoxml

---
+++ LinqToXml/LinqToXml/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace LinqToXml
+{
+    //kod ze strony https://www.tutorialspoint.com/linq/linq_xml.htm
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myXML = @"<Departments>
+                       <Department>Account</Department>
+                       <Department>Sales</Department>
+                       <Department>Pre-Sales</Department>
+                       <Department>Marketing</Department>
+                       </Departments>";
+
+            XDocument xdoc = new XDocument();
+            xdoc = XDocument.Parse(myXML);
+
+            //Add new Element
+            //xdoc.Element("Departments").Add(new XElement("Department", "Finance"));
+
+            //Add new Element at First
+            //xdoc.Element("Departments").AddFirst(new XElement("Department", "Support"));
+
+            //Remove Sales Department
+            //xdoc.Descendants().Where(s => s.Value == "Sales").Remove();
+
+            var result = xdoc.Element("Departments").Descendants();
+
+            foreach (XElement item in result)
+            {
+                Console.WriteLine("Department Name - " + item.Value);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LinqToXml/LinqToXml/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("LinqToXml")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LinqToXml")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("041edbdc-6b3e-463b-9617-bc9684def639")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Rewizja
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
